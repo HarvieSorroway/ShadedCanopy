2866c5fd1fb6fa6621e2be66f0b84c84e3e55dcf
Harvie Sorroway <1934182494@qq.com>
2025-04-08 20:53:40 +0800

    上传文件

---
+++ ShadedCanopy/SCPlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadedCanopy
+{
+    [BepInPlugin(ModID, ModName, ModVersion)]
+    public class SCPlugin : BaseUnityPlugin
+    {
+        public const string ModID = "shaded_canopy";
+        public const string ModName = "Shaded Canopy";
+        public const string ModVersion = "0.0.1";
+
+    }
+}

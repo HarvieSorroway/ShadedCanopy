a8424b4952c6708f664bb9f2212fc8f783b14730
Harvie Sorroway <1934182494@qq.com>
2025-12-23 18:02:40 +0800

    添加了SCTweenContext和SCTweenSequence

---
+++ SCUtils/SCTween/SCTweenSequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCUtils.SCTween
+{
+    public struct SCTweenSequence
+    {
+        List<RwTasks.RwTask> _tweens;
+
+        public SCTweenSequence DoRun(RwTasks.RwTask tweenContextRun)
+        {
+
+            return this;
+        }
+    }
+}
